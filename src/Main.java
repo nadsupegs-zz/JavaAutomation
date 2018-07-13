59092aded5aabdeabe2d32634e79e297003d9b1f
upegu100 <upegu100@mail.chapman.edu>
2018-07-13 14:24:04 -0700

    Example code of opening Chrome browser and closing it using WebDriver

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+
+        //This is just a test that I can open the chrome browser and close it
+        //You must change the file path to wherever your chromedriver.exe is stored
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Nadiya\\Documents\\GrandPad\\JavaAutomatedTesting\\JavaAutomation\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        TimeUnit.SECONDS.sleep(5);
+        driver.quit();
+
+        //For kicks and giggles
         System.out.println("Hello World");
     }
 }
